0210afb9d78ad20c6044e811dadb891582bd5a89
odx <shemgithub@protonmail.com>
2024-03-05 15:59:16 +0800

    niggas
    niggas

--- AssemblyCode.asm
+++ AssemblyCode.asm
@@ -1,10 +1,21 @@
 %include "io64.inc"
 section .data
 var1 db 0
-
+result dq 0
+digit_acc dq 0
 section .text
 global main
 decimal_to_radixn:
+    JMP start
+    error_msg_1:
+    PRINT_STRING "This is an invalid radix, radix must not be greater than 16"
+    NEWLINE
+    JMP start
+    error_msg_2:
+    PRINT_STRING "This is an invalid radix, radix must not be less than 2"
+    JMP start
+    
+    start:
     NEWLINE
 
     PRINT_STRING "Enter a decimal number: "
@@ -13,24 +24,37 @@ decimal_to_radixn:
     
     PRINT_STRING "Enter a Radix: "
     GET_DEC 4, rsi
+    CMP RSI, 16
+    JG error_msg_1
+    CMP RSI, 2
+    JL error_msg_2
+    
 
 
     PRINT_STRING "Decimal: "
     PRINT_DEC 4, RAX
+    cont_div: 
+        xor rdx, rdx               
+        div rsi            
+        
+        mov qword [result], rax   
+        PUSH RDX
+        INC qword [digit_acc]
+        
+        
+        cmp qword [result], 0
+        JNE cont_div
+    NEWLINE
+    PRINT_STRING "N-Radix Result: "
+    pop_stack:
+        
+        POP RBX
+        PRINT_HEX 4, RBX
+        DEC qword [digit_acc]
+        cmp qword [digit_acc], 0
+        JNE pop_stack   
+     
     
-    xor rdx, rdx               
-    div rsi             
-
-    
-    PRINT_STRING "Hexadecimal Quotient: "
-    PRINT_DEC 4, RAX
-
-    ;
-    PRINT_STRING "Hexadecimal Remainder: "
-    PRINT_DEC 1, DL    
-
-    
- 
     ret
 
 radixn_to_decimal:
