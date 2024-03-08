becc5de12ce14af551be9da374a67516ceafa23c
odx <shemgithub@protonmail.com>
2024-03-08 19:21:55 +0800

    Update AssemblyCode.asm
    aaaaa

--- AssemblyCode.asm
+++ AssemblyCode.asm
@@ -1,8 +1,22 @@
 %include "io64.inc"
 section .data
+
+;variables for option 1
 var1 db 0
 result dq 0
 digit_acc dq 0
+
+
+;variables for option2
+input1 dq 0
+input2 dq 0
+accum dq 0
+counter dq 0
+
+quotient_buffer dq 0
+remainder_buffer dq 0 
+current_power dq 1
+
 section .text
 global main
 decimal_to_radixn:
@@ -20,10 +34,10 @@ decimal_to_radixn:
 
     PRINT_STRING "Enter a decimal number: "
 
-    GET_DEC 4, RAX
+    GET_DEC 8, RAX
     
     PRINT_STRING "Enter a Radix: "
-    GET_DEC 4, rsi
+    GET_DEC 8, rsi
     CMP RSI, 16
     JG error_msg_1
     CMP RSI, 2
@@ -32,7 +46,7 @@ decimal_to_radixn:
 
 
     PRINT_STRING "Decimal: "
-    PRINT_DEC 4, RAX
+    PRINT_DEC 8, RAX
     cont_div: 
         xor rdx, rdx               
         div rsi            
@@ -49,7 +63,7 @@ decimal_to_radixn:
     pop_stack:
         
         POP RBX
-        PRINT_HEX 4, RBX
+        PRINT_HEX 8, RBX
         DEC qword [digit_acc]
         cmp qword [digit_acc], 0
         JNE pop_stack   
@@ -58,9 +72,73 @@ decimal_to_radixn:
     ret
 
 radixn_to_decimal:
+    PRINT_STRING "N-radix digits: "
+    GET_HEX 8, RAX
     NEWLINE
-    PRINT_STRING "in radixn to decimal"
     
+    ; Radix, which is in Hex
+    PRINT_STRING "Radix: "
+    GET_DEC 8, RSI
+    NEWLINE
+
+    ; get nth digit, the radix raised it to its position, add it to a sum
+    jmp loop_part
+    
+first_iteration:
+    ADD qword [accum], RDX
+    INC qword [counter]
+
+loop_part:
+    mov r9, 16
+    xor rdx, rdx 
+    div r9
+    CMP RAX, 0
+    JE FINIS
+    MOV qword [remainder_buffer], RDX
+    ; RDX is the digit to raise
+    cmp qword [counter], 0
+    JE first_iteration
+    
+    
+    mov qword [quotient_buffer], RAX ; RAX had quotient
+    mov qword RAX, [current_power]  ;  current power value, so this is 
+    mul RSI
+
+    MOV qword [current_power], RAX
+    
+    MUL qword [remainder_buffer]
+    ADD qword [accum], RAX
+    
+    MOV qword RAX, [quotient_buffer]
+    INC qword [counter]
+    
+   
+    JMP loop_part
+        
+
+
+FINIS:
+    MOV qword [remainder_buffer], RDX
+    ; RDX is the digit to raise
+    cmp qword [counter], 0
+    JE first_iteration
+    
+    
+    mov qword [quotient_buffer], RAX ; RAX had quotient
+    mov qword RAX, [current_power]  ;  current power value, so this is 
+    mul RSI
+
+    MOV qword [current_power], RAX
+    
+    MUL qword [remainder_buffer]
+    ADD qword [accum], RAX
+    
+    
+    MOV qword RAX, [quotient_buffer]
+    INC qword [counter]
+    PRINT_STRING "Decimal equivalent: "
+    PRINT_UDEC 8, [accum]
+    xor rax, rax
     ret
 
 main:
